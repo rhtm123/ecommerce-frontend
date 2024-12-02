acd3efdf4c4c987b51d1d79ffd55eb1b815b539d
rhtm123 <maurya.iitk@gmail.com>
2024-12-02 17:25:18 +0530

    shift to svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+
+<script>
+    import "../app.css";
+    // import Footer from "../components/Footer.svelte";
+    // import Navbar from "../components/Navbar.svelte";
+    import NProgress from 'nprogress';
+    import 'nprogress/nprogress.css';
+    import { navigating } from "$app/stores";
+
+    $: {
+		if ($navigating) {
+			NProgress.start();
+		}
+		if (!$navigating) {
+			NProgress.done();
+		}
+	}
+
+
+</script>
+
+
+<slot />
