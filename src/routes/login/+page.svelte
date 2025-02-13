e73db5e0fe03c598e93d416fe412e9c611aa5f19
Sachin Ghosh <ghoshsachin2301@gmail.com>
2025-02-13 18:26:58 +0530

    Added roting to login page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,5 +1,21 @@
 <script>
     import Login from "$lib/components/Login.svelte";
+    import { user } from '$lib/stores/auth';
+    import { goto } from '$app/navigation';
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        // Check if user is already logged in
+        const unsubscribe = user.subscribe(value => {
+            if (value) {
+                goto('/home'); // Redirect to home if user is logged in
+            }
+        });
+
+        return () => {
+            unsubscribe(); // Cleanup subscription
+        };
+    });
 </script>
 
 <Login />
